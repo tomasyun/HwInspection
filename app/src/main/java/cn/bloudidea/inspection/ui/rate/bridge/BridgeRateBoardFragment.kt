069f53859e65675e4e01e55c995ce4e17160fc1a
yuntuo <yuntuo19900228@126.com>
2020-01-19 14:59:47 +0800

    add ui stateView

--- app/src/main/java/cn/bloudidea/inspection/ui/rate/bridge/BridgeRateBoardFragment.kt
+++ app/src/main/java/cn/bloudidea/inspection/ui/rate/bridge/BridgeRateBoardFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import cn.bloudidea.inspection.R
 import cn.bloudidea.inspection.base.BaseFragment
+import cn.bloudidea.inspection.util.support.EasyStateView
+import kotlinx.android.synthetic.main.fragment_bridge_rate_manage.*
 
 /**
  * 桥梁进度看板
@@ -21,6 +23,6 @@ class BridgeRateBoardFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        bridgeRateBroadStateView.showViewState(EasyStateView.VIEW_EMPTY)
     }
 }
